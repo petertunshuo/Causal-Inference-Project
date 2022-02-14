b8896669272915c309c4ec5f316aa2505be9b244
Peter Lee Jr <ptrlejr@wireless-10-148-72-26.public.utexas.edu>
2022-02-14 00:28:46 -0600

    a

---
+++ R/output/figure_arrests_by_racegender.R
@@ -0,0 +1,19 @@
+# Builds a bar graph with total arrests on the y axis and race/gender on the
+# x axis. Refer to my presentation on graphing for more detail.
+
+library(ggplot2)
+NLSY97_clean <- read.csv("~/Desktop/Causal Inference Data/NLSY97_clean.csv")
+NLSY97_clean%>%
+  group_by(race, gender) %>%
+  summarize(total_arrests = mean(total_arrests)) %>%
+  ggplot(aes(race, total_arrests, fill = gender)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(
+    x = "Race", 
+    y = "Mean Arrests", 
+    fill = "Gender",
+    title = "Mean Number of Arrests in 2002 by Race and Gender") +
+  theme_minimal() +
+  scale_fill_economist()
+
+ggsave("figures/arrests_by_racegender.png", width=8, height=4.5)
