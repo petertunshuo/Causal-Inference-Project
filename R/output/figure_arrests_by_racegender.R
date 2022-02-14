46bbb7250dead62f39add68effe737dc3a6cb793
Peter Lee Jr <ptrlejr@wireless-10-148-72-26.public.utexas.edu>
2022-02-14 16:39:38 -0600

    new graph

--- R/output/figure_arrests_by_racegender.R
+++ R/output/figure_arrests_by_racegender.R
@@ -3,18 +3,18 @@
 
 library(ggplot2)
 library(tidyverse)
-NLSY97_clean <- read.csv("/Causal-Inference-Project/data/NLSY97_clean.csv")
+NLSY97_clean <- read.csv(here("data/NLSY97_clean.csv"))
 NLSY97_clean%>%
   group_by(race, gender) %>%
-  summarize(total_arrests = mean(total_arrests)) %>%
-  ggplot(aes(race, total_arrests, fill = gender)) +
+  summarize(total_incarcerations = mean(total_incarcerations)) %>%
+  ggplot(aes(race, total_incarcerations, fill = gender)) +
   geom_bar(stat = "identity", position = "dodge") +
   labs(
     x = "Race", 
-    y = "Mean Arrests", 
+    y = "Mean Incarcerations", 
     fill = "Gender",
-    title = "Mean Number of Arrests in 2002 by Race and Gender") +
+    title = "Mean Number of Incarcerations in 2002 by Race and Gender") +
   theme_minimal() +
   scale_fill_economist()
 
-ggsave("figures/arrests_by_racegender.png", width=8, height=4.5)
+ggsave("figures/incarcerations_by_racegender.png", width=8, height=4.5)
